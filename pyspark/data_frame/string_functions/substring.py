ef42acefd82c2e900ebfb22f59ea5ee9abbb26dd
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-26 19:17:13 +0530

    substring

---
+++ pyspark/data_frame/string_functions/substring.py
@@ -0,0 +1,14 @@
+from pyspark.sql import *
+from pyspark.sql.functions import  *
+
+spark = SparkSession.builder.appName('trim').getOrCreate()
+
+name = [("Aarav", "Patel", "Udaipur"),
+        ("Aadya", "Sharma", "Bhopal"),
+        ("Advik", "Verma", "Indore")]
+
+schema = ["first_name", "last_name", "city"]
+
+df_name = spark.createDataFrame(data=name, schema=schema)
+df_name = df_name.withColumn("last_name", substring(col("last_name"), 1,4))
+df_name.show()
