e2c1e992be7ea287850e850c7bb7fe7a010bd182
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-26 19:17:42 +0530

    traslate

---
+++ pyspark/data_frame/string_functions/translate.py
@@ -0,0 +1,14 @@
+from pyspark.sql import *
+from pyspark.sql.functions import  col , translate
+
+spark = SparkSession.builder.appName('trim').getOrCreate()
+
+name = [("Aarav", "Patel", "Udaipur"),
+        ("Aadya", "Sharma", "Bhopal"),
+        ("Advik", "Verma", "Indore")]
+
+schema = ["first_name", "last_name", "city"]
+
+df_name = spark.createDataFrame(data=name, schema=schema)
+df_name = df_name.withColumn("city" , translate(col("city"), "aeiou" , "12345"))
+df_name.show()
