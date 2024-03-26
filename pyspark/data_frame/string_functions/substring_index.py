12f7d682b57976892a78ef460b5bf6a0e5625788
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-26 19:17:23 +0530

    substring index

---
+++ pyspark/data_frame/string_functions/substring_index.py
@@ -0,0 +1,18 @@
+
+
+from pyspark.sql import *
+from pyspark.sql.functions import  col , substring_index
+
+spark = SparkSession.builder.appName('trim').getOrCreate()
+
+name = [("Aarav", "Patel", "Udaipur"),
+        ("Aadya", "Sharma", "Bhopal"),
+        ("Advik", "Verma", "Indore")]
+
+schema = ["first_name", "last_name", "city"]
+
+df_name = spark.createDataFrame(data=name, schema=schema)
+df_name = df_name.withColumn("substring1", substring_index(col("first_name"), "a", 1))
+#if we use -1 then it will print substring after a
+df_name = df_name.withColumn("substring2", substring_index(col("first_name"),"a", -1))
+df_name.show()
