9f0e3e00465eca8a01b997777c9a25ec2b959271
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-28 19:07:40 +0530

    read json file

--- pyspark/data_frame/read_files/read_json_file.py
+++ pyspark/data_frame/read_files/read_json_file.py
@@ -4,3 +4,5 @@ spark = SparkSession.builder.appName('Pratibha').getOrCreate()
 df = spark.read.option("multiline", "true").json("sample4.json")
 df.show(truncate=False)
 df.printSchema()
+
+# reading the json with custom schema
