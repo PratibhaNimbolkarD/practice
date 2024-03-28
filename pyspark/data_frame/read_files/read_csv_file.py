3a586a91f0ddfb42ac7fe2b614d1d95a894f8583
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-03-28 19:07:28 +0530

    read csv file

--- pyspark/data_frame/read_files/read_csv_file.py
+++ pyspark/data_frame/read_files/read_csv_file.py
@@ -1,6 +1,11 @@
 from pyspark.sql import *
 
 spark = SparkSession.builder.appName('Pratibha').getOrCreate()
-df = spark.read.csv(path=r'C:\Users\PratibhaNimbolkar\Downloads\customers-100.csv' , header=True)
+
+
+
+df = spark.read.csv(path=r'C:\Users\PratibhaNimbolkar\Downloads\customers-100.csv' , header=True , inferSchema=True)
 df.show()
 df.printSchema()
+
+
