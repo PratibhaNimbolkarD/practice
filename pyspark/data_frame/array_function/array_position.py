a1576c746c51b5b334fac47e47ac664708889d42
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-04-01 21:31:47 +0530

    array_position

---
+++ pyspark/data_frame/array_function/array_position.py
@@ -0,0 +1,20 @@
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType
+from pyspark.sql.functions import array_position
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("Array Functions").getOrCreate()
+
+data = [("Amitabh", ["Physics", "Chemistry", "Maths"]),
+        ("Priyanka", ["Physics", "Maths"]),
+        ("Aishwarya", ["Chemistry", "Biology", "Physics"]),
+        ("Shahrukh", ["Accounts", "Biology", "Chemisty"]),
+        ("Deepika", ["Chemistry", "Physics"])]
+
+schema = StructType([
+    StructField("Name", StringType(), True),
+    StructField("Subject", ArrayType(StringType()), True)
+])
+
+df = spark.createDataFrame(data, schema)
+
+result_df = df.withColumn("position_of_physics", array_position("Subject", "Physics"))
+result_df.show(truncate=False)
