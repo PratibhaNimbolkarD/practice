93d9cc25f9d5cf835799a62917d8b393978e9614
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-04-01 20:39:35 +0530

    explode outer

---
+++ pyspark/data_frame/explode_array_and_map_functions/explode_outer.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode_outer
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType
+
+
+spark = SparkSession.builder.appName("explode").getOrCreate()
+
+data = [
+    ("Aakash", 25, ["apple", "banana", "orange"], "New York", "Male"),
+    ("Himani", 30, ["grape", "pear"], "Los Angeles", "Female"),
+    ("Neha", 35, ["watermelon", "kiwi", "mango"], "Chicago", "Female"),
+    ("Junaid", 40, ["pineapple", "strawberry"], "Houston", "Male"),
+    ("Manisha", 28, ["blueberry", "raspberry", "blackberry"], "San Francisco", "Female")
+]
+
+schema = StructType([
+    StructField("Name", StringType(), True),
+    StructField("Age", IntegerType(), True),
+    StructField("Fruits", ArrayType(StringType()), True),
+    StructField("Location", StringType(), True),
+    StructField("Gender", StringType(), True)
+])
+
+df = spark.createDataFrame(data=data , schema=schema)
+exploded_outer_df = df.select("Name", "Age", explode_outer("Fruits").alias("Fruit"), "Location", "Gender")
+exploded_outer_df.show()
