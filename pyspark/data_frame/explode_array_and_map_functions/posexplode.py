cf16e0d1f558920b5d65d61e581284ea9b87f312
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-04-01 20:41:10 +0530

    posexplode

---
+++ pyspark/data_frame/explode_array_and_map_functions/posexplode.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import posexplode
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, ArrayType
+
+spark = SparkSession.builder.appName("explode").getOrCreate()
+
+data = [
+    ("Aakash", 25, ["apple", "banana", "orange"], "New York", "Male"),
+    ("Himani", 30, ["grape", "pear"], "Los Angeles", "Female"),
+    ("Neha", 35, ["watermelon", "kiwi", "mango"], "Chicago", "Female"),
+    ("Junaid", 40, ["pineapple", "strawberry"], "Houston", "Male"),
+    ("Manisha", 28, ["blueberry", "raspberry", "blackberry"], "San Francisco", "Female")
+]
+
+schema = StructType([
+    StructField("Name", StringType(), True),
+    StructField("Age", IntegerType(), True),
+    StructField("Fruits", ArrayType(StringType()), True),
+    StructField("Location", StringType(), True),
+    StructField("Gender", StringType(), True)
+])
+
+df = spark.createDataFrame(data=data , schema=schema)
+
+posexploded_df = df.select("Name", "Age", posexplode("Fruits").alias("Pos", "Fruit"), "Location", "Gender")
+posexploded_df.show()
