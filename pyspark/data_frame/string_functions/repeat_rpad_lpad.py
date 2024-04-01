01f390c65022a5a7f6c8e9a2a39e6a1edc760e3a
Pratibha Nimbolkar <pratibha.nimbolkar@diggibyte.com>
2024-04-01 20:43:43 +0530

    repeat rpad lpad

--- pyspark/data_frame/string_functions/repeat_rpad_lpad.py
+++ pyspark/data_frame/string_functions/repeat_rpad_lpad.py
@@ -11,9 +11,12 @@ schema = ["first_name", "last_name", "city"]
 
 df_name = spark.createDataFrame(data=name, schema=schema)
 
-df = df_name.withColumn("last_name", repeat(col("last_name"), 2))
-df1 = df.withColumn("first_name", rpad(col("first_name"), 10, "0"))
-df2 = df1.withColumn("city", lpad(col("city"), 10, "*"))
-df.show()
-df1.show()
-df2.show()
+df_repeat = df_name.withColumn("last_name", repeat(col("last_name"), 2))
+df_rpad = df_name.withColumn("first_name", rpad(col("first_name"), 10, "0"))
+df_lpad = df_name.withColumn("city", lpad(col("city"), 10, "*"))
+print("using repeat function")
+df_repeat.show()
+print("using rpad function")
+df_rpad.show()
+print("using lpad function")
+df_lpad.show()
